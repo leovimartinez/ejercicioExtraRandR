bf2ce62642c864ebe5822ab78dc4ac7de8c73628
leovimartinez <leovioz@hotmail.com>
2016-08-14 02:00:58 -0500

    Commit con persistencia y validaciones del formulario de usuario

--- Gemfile
+++ Gemfile
@@ -45,3 +45,6 @@ group :development do
   gem 'spring'
 end
 
+  # Gem Bootstrap
+  gem 'bootstrap-sass'
+  gem 'bootswatch-rails'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def formulario
   end
+
+  def guardaUsuario
+  	Usuario.create(nombre: params[:nombre], edad: params[:edad], correo: params[:correo])
+  	redirect_to pages_formulario_path, notice: "Su informacio ha sido guardada"
+  end
 end
